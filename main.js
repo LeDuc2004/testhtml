90443a9066bda4e1f185505fdab7902a00ee4273
LeDuc2004 <levanduc2052004@gmail.com>
2024-04-11 19:58:53 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,65 @@
+const scrollers = document.querySelectorAll(".scroller");
+
+$(document).ready(function () {
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
+  }
+
+  function addAnimation() {
+    $(".scroller").each(function () {
+      $(this).attr("data-animated", true);
+
+      var scrollerInner = $(this).find(".scroller__inner");
+      var scrollerContent = scrollerInner.children().toArray();
+
+      $.each(scrollerContent, function () {
+        var duplicatedItem = $(this).clone(true);
+        duplicatedItem.attr("aria-hidden", true);
+        scrollerInner.append(duplicatedItem);
+      });
+    });
+  }
+
+  var scrollersH = $(".scroller_hero");
+
+  if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimationH();
+  }
+
+  function addAnimationH() {
+    scrollersH.each(function () {
+      $(this).attr("data-animated", true);
+
+      var scrollerInner = $(this).find(".scroller_hero__inner");
+      var scrollerContent = scrollerInner.children().toArray();
+
+      $.each(scrollerContent, function () {
+        var duplicatedItem = $(this).clone(true);
+        duplicatedItem.attr("aria-hidden", true);
+        scrollerInner.append(duplicatedItem);
+      });
+    });
+  }
+
+  $(document).ready(function () {
+    $("#wrap_dot li").on("click", function () {
+      $("#wrap_dot li").removeClass("active");
+
+      $(this).addClass("active");
+    });
+
+    $("#hero_two").on("click", function () {
+      $(".hero video").css("opacity", 1);
+      $(".hero .overlay_video").css("opacity", 1);
+      $(".hero .hero_video_text").css("opacity", 1);
+
+    });
+
+    $("#hero_one").on("click", function () {
+      $(".hero video").css("opacity", 0);
+      $(".hero .overlay_video").css("opacity", 0);
+      $(".hero .hero_video_text").css("opacity", 0);
+
+    });
+  });
+});
