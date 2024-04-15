9095d78325a66745f932cacf97cd4b585ef04161
LeDuc2004 <levanduc2052004@gmail.com>
2024-04-16 06:48:11 +0700

    add responsive for mobile

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ $(document).ready(function () {
 
   $(document).ready(function () {
     setTimeout(() => {
-      $("#hero_two").click()
+      $("#hero_two").click();
     }, 3000);
     $("#wrap_dot li").on("click", function () {
       $("#wrap_dot li").removeClass("active");
@@ -63,29 +63,53 @@ $(document).ready(function () {
       $(".hero .overlay_video").css("opacity", 0);
       $(".hero .hero_video_text").css("opacity", 0);
       $(".hero .hero_desription").css("pointer-events", "all");
-
     });
   });
 
   $(".multiple-items").slick({
-    infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 1.15,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: false,
+        },
+      },
+    ],
   });
 
   $(".customer-items").slick({
     slidesToShow: 3,
     slidesToScroll: 1,
     dots: true,
+    responsive: [
+      {
+        breakpoint: 1000,
+        settings: {
+          slidesToShow: 1.18,
+          slidesToScroll: 1,
+          dots: true,
+          infinite: false,
+        },
+      },
+    ],
   });
 
   $("#download_profile_btn").on("click", () => {
-    onShowPopup("#download_modal");
+    $("html, body").animate({ scrollTop: 0 }, "slow", () => {
+      onShowPopup("#download_modal");
+    });
   });
 
   $("#consult_first_btn").on("click", () => {
-    onShowPopup("#consult_modal");
+    $("html, body").animate({ scrollTop: 0 }, "slow", () => {
+      onShowPopup("#consult_modal");
+    });
   });
 
   $(".multiple-items .item .see_more_btn").on("click", () => {
@@ -100,7 +124,49 @@ $(document).ready(function () {
 
   $(".close_modal_btn").on("click", () => {
     onHidePopup("#download_modal");
+    onHidePopup("#consult_modal");
   });
+  $(".head_sp_back").on("click", () => {
+    onHidePopup("#download_modal");
+    onHidePopup("#consult_modal");
+  });
+
+  // header mobile
+
+  $("#burger").on("click", (event) => {
+    let $this = $(event.currentTarget);
+    if ($this.hasClass("show")) {
+      $this.removeClass("show");
+      $this.find(".close").hide();
+      $this.find(".burger").show();
+      $(".dropdown-content-head").removeClass("show");
+    } else {
+      $this.addClass("show");
+      $this.find(".close").show();
+      $this.find(".burger").hide();
+      $(".dropdown-content-head").addClass("show");
+    }
+  });
+
+  $(".accordion").on("click", function (e) {
+    const activePanel = $(e.target).closest(".accordion-panel");
+    if (!activePanel.length) return;
+    toggleAccordion(activePanel);
+  });
+
+  function toggleAccordion(panelToActivate) {
+    const activeButton = panelToActivate.find(".accordion-trigger");
+    const activePanel = panelToActivate.find(".accordion-content");
+    const activePanelIsOpened = activeButton.attr("aria-expanded");
+
+    if (activePanelIsOpened === "true") {
+      activeButton.attr("aria-expanded", false);
+      activePanel.attr("aria-hidden", true);
+    } else {
+      activeButton.attr("aria-expanded", true);
+      activePanel.attr("aria-hidden", false);
+    }
+  }
 
   function onShowPopup(modalName) {
     $("body").addClass("modal_Open");
@@ -112,4 +178,51 @@ $(document).ready(function () {
     $("#over_lay").removeClass("show");
     $(modalName).removeClass("show");
   }
+
+        // Khởi tạo Odometer
+        const odometer1 = new Odometer({
+          el: document.querySelector("#our_customers"),
+          value: 10,
+        });
+        const odometer2 = new Odometer({
+          el: document.querySelector("#successfull_projects"),
+          value: 10,
+        });
+        const odometer3 = new Odometer({
+          el: document.querySelector("#offices"),
+          value: 1,
+        });
+        const odometer4 = new Odometer({
+          el: document.querySelector("#ict_vn"),
+          value: 1,
+        });
+        const odometer5 = new Odometer({
+          el: document.querySelector("#customer_sati"),
+          value: 10,
+        });
+        const odometer6 = new Odometer({
+          el: document.querySelector("#customer"),
+          value: 10,
+        });
+  
+        window.addEventListener("scroll", function () {
+          var element = document.getElementById("myOdometer");
+          var position = element.getBoundingClientRect();
+          var windowHeight = window.innerHeight;
+  
+          if (position.top < windowHeight * 0.8) {
+            activateOdometer();
+            window.removeEventListener("scroll", arguments.callee);
+          }
+        });
+  
+        // Hàm kích hoạt odometer
+        function activateOdometer() {
+          odometer1.update(60);
+          odometer2.update(100);
+          odometer3.update(2);
+          odometer4.update(10);
+          odometer5.update(95);
+          odometer6.update(91);
+        }
 });
